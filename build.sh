606774c8da6f61860264ef6fe062fbc1f807cc60
Lewis Van Winkle <lv@codeplea.com>
2018-09-18 13:30:07 -0500

    added unix_list.c

--- build.sh
+++ build.sh
@@ -1,10 +1,20 @@
-SRC=id
+echo
+cp id.c id.cpp
+${CC} -Wall -Wextra id.c -o id; ./id; rm id
+echo
+${CC} -Wall -Wextra id.cpp -o id; ./id; rm id
+rm id.cpp
+echo
+echo
 
+
+cd chap01
 echo
-cp ${SRC}.c ${SRC}.cpp
-${CC} -Wall -Wextra ${SRC}.c -o ${SRC}; ./${SRC}; rm ${SRC}
+cp unix_list.c unix_list.cpp
+${CC} -Wall -Wextra unix_list.c -o unix_list; ./unix_list; rm unix_list
 echo
-${CC} -Wall -Wextra ${SRC}.cpp -o ${SRC}; ./${SRC}; rm ${SRC}
-rm ${SRC}.cpp
+${CC} -Wall -Wextra unix_list.cpp -o unix_list; ./unix_list; rm unix_list
+rm unix_list.cpp
 echo
 echo
+cd ..
