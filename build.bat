99a01187c9c2d91a806d83e6ab20c5178754775c
Lewis Van Winkle <lv@codeplea.com>
2018-09-18 12:55:23 -0500

    updated appveyor

--- build.bat
+++ build.bat
@@ -1,9 +1,9 @@
 copy id.c id.cpp
 echo "%CC%"
-%CC% id.c %CEXTRA%
+%CC% id.c -o id.exe %CEXTRA%
 id.exe
 del id.exe
-%CC% id.cpp %CEXTRA%
+%CC% id.cpp -o id.exe %CEXTRA%
 id.exe
 del id.exe
 del id.cpp
@@ -12,10 +12,10 @@ del id.cpp
 cd chap01
 copy win_init.c win_init.cpp
 echo "%CC%"
-%CC% win_init.c %CEXTRA%
+%CC% win_init.c -o win_init.exe %CEXTRA%
 win_init.exe
 del win_init.exe
-%CC% win_init.cpp %CEXTRA%
+%CC% win_init.cpp -o win_init.exe %CEXTRA%
 win_init.exe
 del win_init.exe
 del win_init.cpp
@@ -25,10 +25,10 @@ cd ..
 cd chap01
 copy win_list.c win_list.cpp
 echo "%CC%"
-%CC% win_list.c %CEXTRA%
+%CC% win_list.c -o win_list.exe %CEXTRA%
 win_list.exe
 del win_list.exe
-%CC% win_list.cpp %CEXTRA%
+%CC% win_list.cpp -o win_list.exe %CEXTRA%
 win_list.exe
 del win_list.exe
 del win_list.cpp
