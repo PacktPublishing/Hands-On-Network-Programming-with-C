b078f71e2cc16b96870803470852a663ae7bf4d6
Lewis Van Winkle <lv@codeplea.com>
2019-03-27 18:23:45 -0500

    remove nl from error text

--- chap13/error_text.c
+++ chap13/error_text.c
@@ -33,6 +33,8 @@ const char *get_error_text() {
     FormatMessage(
         FORMAT_MESSAGE_FROM_SYSTEM|FORMAT_MESSAGE_IGNORE_INSERTS,
         0, WSAGetLastError(), 0, message, 256, 0);
+    char *nl = strrchr(message, '\n');
+    if (nl) *nl = 0;
     return message;
 
 #else
